ce5137180773991c6885398f9c4069f8a8fd19bd
rmontone <rmontone19@gmail.com>
2023-07-26 14:08:14 -0300

    criando primeiro .go

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nome = "Henrique"
+	version := 1.8
+	fmt.Println("Hello, Mr.", nome)
+	fmt.Println("This program is in the", version, "version")
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variavel comando é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
+}
