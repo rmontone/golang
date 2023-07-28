dc39883625500a1e254b420aa5c1fa6242dc90c7
rmontone <rmontone19@gmail.com>
2023-07-28 00:18:43 -0300

    aula 02

--- hello.go
+++ hello.go
@@ -2,20 +2,49 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+
+	exibeIntroducao()
+
+	exibeMenu()
+
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço esse comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
 	nome := "Henrique"
 	version := 1.8
 	fmt.Println("Hello, Mr.", nome)
 	fmt.Println("This program is in the", version, "version")
+}
 
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
+}
+
+func exibeMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do programa")
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O endereço da minha variável comando é", &comando)
-	fmt.Println("O comando escolhido foi", comando)
 }
