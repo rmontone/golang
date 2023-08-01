ac46e8c512210b3c115ac1f6050337f6204f8255
rmontone <rmontone19@gmail.com>
2023-08-01 20:33:03 -0300

    for e arrays

--- hello.go
+++ hello.go
@@ -4,28 +4,34 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const delay = 5
 
 func main() {
 
 	exibeIntroducao()
 
-	exibeMenu()
+	for {
+		exibeMenu()
 
-	comando := leComando()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		exibeLogs()
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço esse comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			exibeLogs()
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço esse comando")
+			os.Exit(-1)
+		}
 	}
+
 }
 
 func exibeIntroducao() {
@@ -52,16 +58,29 @@ func exibeMenu() {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://random-status-code.herokuapp.com"
-	resp, _ := http.Get(site)
-	if resp.StatusCode == 200 {
-		fmt.Println("Site: ", site, " foi carregado com sucesso!")
-	} else {
-		fmt.Println("Site: ", site, "está com problemas.\nStatus Code: ", resp.StatusCode)
-	}
+	testaSites()
+	time.Sleep(delay * time.Second)
+
 }
 
 func exibeLogs() {
 	fmt.Println("Exibindo Logs...")
 
+}
+func testaSites() {
+	fmt.Println("------------------------------------------------------------------------")
+	sites := [](string){"https://www.fiap.com.br", "https://cursos.alura.com.br/dashboard", "https://github.com/codecrafters-io/build-your-own-x#build-your-own-operating-system", "https://flaviocopes.com/go-git-contributions/"}
+	for i, site := range sites {
+
+		resp, _ := http.Get(site)
+		if resp.StatusCode == 200 {
+			fmt.Println("Site", i+1, ":")
+			fmt.Println("Site: ", site, " foi carregado com sucesso!\nStatus Code: ", resp.StatusCode)
+			fmt.Println("\n-------------------------------------------------------------------------------")
+		} else {
+			fmt.Println(i + 1)
+			fmt.Println("Site: ", site, "está com problemas.\nStatus Code: ", resp.StatusCode)
+			fmt.Println("\n-------------------------------------------------------------------------------")
+		}
+	}
 }
